b99f2e70dcfcabd41339724fdceb93e8f30f6af3
Kevin Masson <contact.kevinmasson@gmail.com>
2018-02-28 21:20:18 +0100

    Change structure

--- hashcode-2017/main.cpp
+++ hashcode-2017/main.cpp
@@ -15,7 +15,7 @@ typedef std::map<int, int> EndPointToCentral;
 
 // Lantency from 1 endpoint to servers
 // First: ServerId, Second: Latency
-typedef std::map<int, int> EndPointToServers;
+typedef std::vector<std::pair<int, int>> endPointToServers;
 // Lantency from all endpoint to servers
 typedef std::map<int, EndPointToServers> AllEndPointToServers;
 
@@ -27,7 +27,6 @@ typedef std::map<int, int> Requests;
 // First id endpoint, Second: Requests
 typedef std::map<int, Requests> EndpointRequests;
 
-
 // Algo structs
 
 // Tell for [A Server Id] [Capcaity Left]
@@ -36,6 +35,47 @@ typedef std::map<int, int> RemainingCapacity;
 // Out structs
 // [Server ID] [VideoId, VideoID....]
 typedef std::map<int, std::vector<int>> AssignedServerVideos;
+
+VideoSizes videoSizes;
+CacheServers cacheServers;
+EndPointToCentral endPointToCentral;
+EndPointToServers endPointToServers;
+AllEndPointToServers allEndPointToServers;
+Requests requests;
+EndpointRequests endpointRequests;
+RemainingCapacity remainingCapacity;
+AssignedServerVideos assignedServerVideos;
+int video = -1; // Current video of the request
+int endpoint = -1; // Current endpoint of the request
+int max = -1; // Value (size * count) of the request
+int test; // Temp value
+int serverid; // Id du meilleur serveur pour l'ajout
+int min_latency; // Meilleur latence trouvee pour un endpoint
+
+void trouver_meilleur_request()
+{
+    // trouver request max (count * size)
+    // We ignore values above this
+    for(auto & endpoint_request : endpointRequests)
+    {
+        for(auto & video_count : endpoint_request.second)
+        {
+            if(video_count.second == -1) continue;
+            test = video_count.second * videoSizes[video_count.first];
+            if(test > max && test < threshold)
+            {
+                max = test;
+                endpoint = endpoint_request.first;
+                video = video_count.first;
+            }
+        }
+    }
+}
+
+void trouver_meilleur_serveur()
+{
+
+}
 
 void read(const std::string &filename, VideoSizes &videoSizes,
           EndPointToCentral &endPointToCentral, AllEndPointToServers &allEndPointToServers,
@@ -84,16 +124,6 @@ void read(const std::string &filename, VideoSizes &videoSizes,
 
 int main()
 {
-    VideoSizes videoSizes;
-    CacheServers cacheServers;
-    EndPointToCentral endPointToCentral;
-    EndPointToServers endPointToServers;
-    AllEndPointToServers allEndPointToServers;
-    Requests requests;
-    EndpointRequests endpointRequests;
-    RemainingCapacity remainingCapacity;
-    AssignedServerVideos assignedServerVideos;
-
     read("me_at_the_zoo.in", videoSizes, endPointToCentral, allEndPointToServers, endpointRequests);
 
     // Fill remaining capacities
@@ -102,29 +132,20 @@ int main()
         remainingCapacity[kv.first] = kv.second;
     }
 
-    // Do algo
-
-    // trouver request max (count * size)
-    int video = 0;
-    int endpoint = 0;
-    int max = 0;
-    // We ignore values above this
-    int threshold = std::numeric_limits<int>::max();
-    int test;
-    for(auto & endpoint_request : endpointRequests)
+    // Tant qu'il n'y plus de requests a traiter
+    do
     {
-        for(auto & video_count : endpoint_request.second)
-        {
-            test = video_count.second * videoSizes[video_count.first];
-            if(test > max && test < threshold)
-            {
-                max = test;
-                endpoint = endpoint_request.first;
-                video = video_count.first;
-            }
-        }
-    }
-    threshold = max;
+        video = -1;
+        endpoint = -1;
+
+        trouver_meilleur_request();
+        trouver_meilleur_serveur();
+
+        // Modifier request pour ne plus passer dessus
+        endpointRequests[endpoint][video] = -1;
+    } while(video != -1);
+
+
 
     return 0;
 }
